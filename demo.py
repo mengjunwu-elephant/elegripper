a6a08eb7c18b630c66d08ed60ac423a1d3e36a31
TheFive666 <116151187+TheFive666@users.noreply.github.com>
2024-09-12 12:00:18 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 from elegripper import Gripper
 import time
 if __name__=="__main__":
-    g=Gripper("COM8",115200)##Enter the actual serial port number
+    g=Gripper("COM8",115200)##填写实际的串口号和波特率
     g.set_gripper_value(100,100)
     time.sleep(2)
     g.set_gripper_value(0,100)
