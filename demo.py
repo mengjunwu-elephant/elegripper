e7064f039689b319947730f0f3ce4e49049d2572
TheFive666 <116151187+TheFive666@users.noreply.github.com>
2024-09-12 11:50:37 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 from elegripper import Gripper
 import time
 if __name__=="__main__":
-    g=Gripper("COM8")##Enter the actual serial port number
+    g=Gripper("COM8",115200)##Enter the actual serial port number
     g.set_gripper_value(100,100)
     time.sleep(2)
     g.set_gripper_value(0,100)
