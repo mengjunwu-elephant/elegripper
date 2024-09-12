c6e9929fb0b7a3277c2e984f46a66d911c61aeb0
TheFive666 <116151187+TheFive666@users.noreply.github.com>
2024-09-12 15:43:44 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,11 +1,11 @@
 from elegripper import Gripper
 import time
 if __name__=="__main__":
-    g=Gripper("COM8",115200,1)##填写实际的串口号和波特率和夹爪ID
+    g=Gripper("COM27",115200,1)##填写实际的串口号和波特率和夹爪ID
     print("夹爪的实际ID为:",g.get_gripper_Id())
-    g.set_gripper_value(100,100)
+    print(g.set_gripper_value(100,100))
     time.sleep(2)
-    g.set_gripper_value(0,100)
+    print(g.set_gripper_value(0,100))
     time.sleep(2)
     
 
